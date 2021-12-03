bddd242585e8998f2716b5284568a076b9fb94ba
TanyaTrue <tatianatrue04@gmail.com>
2021-12-03 22:33:56 +0300

    renamed backgrounds, refactored

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 const countdown = document.getElementById("countdown");
 const year = document.getElementById("year");
-const  loading = document.getElementById("loading");
+const loading = document.getElementById("loading");
 const currentYear = new Date().getFullYear();
 
 const NewYearTime = new Date(`January 01 ${currentYear + 1} 00: 00: 00`);
